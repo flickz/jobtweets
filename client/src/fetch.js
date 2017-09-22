a7fe6135ba2c6461abd949ce55eaa49e244fc156
flickz <omoyajowo2015@gmail.com>
2017-09-22 23:17:43 +0100

    updated socket url

--- client/src/fetch.js
+++ client/src/fetch.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const stream =  io('https://jobtweets', {secure: true});
+const stream =  io();
 
 stream.on('error', (error)=>{
   console.log("Error occoured..");
